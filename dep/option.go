4c4d61c6892a0927280bd181a98a7be1c836075f
heffcodex <dev@heffcodex.pro>
2022-12-14 01:03:40 +0400

    init

--- dep/option.go
+++ dep/option.go
@@ -12,9 +12,9 @@ func Singleton[T any]() Option[T] {
 	}
 }
 
-func Debug[T any](l *zap.Logger) Option[T] {
+func Debug[T any](enable bool, l *zap.Logger) Option[T] {
 	return func(d *D[T]) {
-		d.debug = true
+		d.debug = enable
 		d.debugLog = l
 	}
 }
